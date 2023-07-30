5b53a9592d2fc962975ce98338b1bede0a66ff57
Nguyen Van Dung <zus@Nguyens-MacBook-Pro.local>
2023-07-30 11:22:53 +0700

    use ngrok

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,8 +83,7 @@ export default function Chat(props: { apiKeyApp: string }) {
     setLoading(true);
 
     const url =
-      process.env.URL ||
-      'http://ec2-18-143-177-11.ap-southeast-1.compute.amazonaws.com/ask';
+      process.env.URL || 'https://c2c0-101-99-14-10.ngrok-free.app/ask';
     const response = await fetch(url, {
       method: 'POST',
       headers: {
